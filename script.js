02b201381cdf05966b30cda2ce86272fc199c1e2
IvanR96 <irios96@hotmail.com>
2024-02-08 21:42:31 -0800

    final touch

--- script.js
+++ script.js
@@ -12,28 +12,20 @@ const btn = document.querySelector('button');
 
 
 
-btn.addEventListener('click', function(){
+btn.addEventListener('click', function(event){
+    event.preventDefault();
 
     const container = document.getElementById('container');
-    
     const prodDiv = document.getElementById('product');
-
-    
     const productName = document.getElementById('names').value;
     const productPrice = document.getElementById('price').value;
-
     const newProduct = new Product(productName, productPrice);
 
-
-
     const text = document.createElement('p');
-
     text.innerHTML = (`You have entered ${newProduct.name} with a price tag of $${newProduct.price}`);
-
+    
     prodDiv.appendChild(text);
-
     container.appendChild(prodDiv);
-
 
 
 });
