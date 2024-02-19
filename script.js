93a33b4525e659ec8fecad7e7b813b91c186a23e
IvanR96 <irios96@hotmail.com>
2024-02-18 23:23:31 -0800

    added datenow to return elapsed time

--- script.js
+++ script.js
@@ -37,9 +37,9 @@ taskForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
 
-  const taskObj ={
-    id: `${titleInput.value.toLowerCase().split(' ').join('-')}`,
-  }
+  const taskObj = {
+    id: `${titleInput.value.toLowerCase().split(' ').join('-')}-${Date.now()}`
+  };
 });
 
 
