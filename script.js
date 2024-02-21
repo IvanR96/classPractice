3625dc2b8ca3852616fce27bb202ede3ce8618d1
IvanR96 <irios96@hotmail.com>
2024-02-20 16:27:08 -0800

    delete task added

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ const addOrUpdateTask = () => {
 
 const updateTaskContainer = () => {
   tasksContainer.innerHTML = "";
-  
+
   taskData.forEach(
     ({ id, title, date, description }) => {
         (tasksContainer.innerHTML += `
@@ -41,13 +41,22 @@ const updateTaskContainer = () => {
           <p><strong>Title:</strong> ${title}</p>
           <p><strong>Date:</strong> ${date}</p>
           <p><strong>Description:</strong> ${description}</p>
-          <button type="button" class="btn">Edit</button>
-          <button type="button" class="btn">Delete</button>
+          <button type="button" onclick="editTask(this)"class="btn">Edit</button>
+          <button type="button" onclick="deleteTask(this)" class="btn">Delete</button> 
         </div>
       `)
     } 
   );
 };
+
+const deleteTask = (buttonEl) => {
+  const dataArrIndex = taskData.findIndex(
+    (item) => item.id === buttonEl.parentElement.id
+  );
+
+  buttonEl.parentElement.remove();
+  taskData.splice(dataArrIndex, 1);
+}
 
 const reset = () => {
   titleInput.value = '';
